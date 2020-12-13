ac1c6648a92fa6efe6799a9f7bdf1c91744accba
Yichar <597297146@qq.com>
2020-12-13 10:13:00 +0800

    104

--- OJ/bin_Tree/leecode_104最大深度.cpp
+++ OJ/bin_Tree/leecode_104最大深度.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //递归
 class Solution {
 public:
     int getDepth(TreeNode *root){
@@ -19,5 +20,31 @@ public:
     }
     int maxDepth(TreeNode* root) {
         return getDepth(root);
+    }
+};
+
+//迭代
+class Solution {
+public:
+ 
+    queue<TreeNode *> Q;
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        Q.push(root);
+        int ans = 0;
+        while(!Q.empty()) {
+            int size = Q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode *temp = Q.front();
+                Q.pop();
+           
+                if( temp->left != nullptr)
+                    Q.push(temp->left);
+                if(temp->right != nullptr)
+                    Q.push(temp->right);
+            } 
+            ans++;
+        }
+        return ans;
     }
 };
